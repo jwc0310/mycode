fdc7b378de9de7da68b391802548d5fb27c5f887
jwc0310 <jwc0310@163.com>
2015-10-12 16:05:00 +0800

    Add A Thread For Showing Rotate Function

--- EditText/src/com/example/mygraphics/GameViewRotateActivity.java
+++ EditText/src/com/example/mygraphics/GameViewRotateActivity.java
@@ -2,8 +2,9 @@ package com.example.mygraphics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class GameViewRotateActivity extends Activity {
+public class GameViewRotateActivity extends Activity  {
 	
 	private GameViewRotate mGvr;
 	
@@ -14,6 +15,23 @@ public class GameViewRotateActivity extends Activity {
 		mGvr = new GameViewRotate(this);
 		setContentView(mGvr);
 		
+		new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while(!Thread.currentThread().isInterrupted()){
+					try{
+						Thread.sleep(3000);
+					}catch(InterruptedException e){
+						Thread.currentThread().interrupt();
+					}
+					mGvr.Angle+=45;
+				}
+				
+			}
+			
+		}).start();
 		
 		
 	}
