a842fac5a2601dd5f57a821ed4295b4569612a38
jwc0310 <jwc0310@163.com>
2015-10-13 15:35:45 +0800

    Set FullScreen Show

--- EditText/src/com/example/mygraphics/GameViewDoubleBufferActivity.java
+++ EditText/src/com/example/mygraphics/GameViewDoubleBufferActivity.java
@@ -1,9 +1,12 @@
 package com.example.mygraphics;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GameViewDoubleBufferActivity extends Activity {
 
@@ -12,6 +15,15 @@ public class GameViewDoubleBufferActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		
+		//设置无标题栏
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		//设置全屏模式
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		//设置为横屏
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		
 		mGvdb = new GameViewDoubleBuffer(this);
 		
